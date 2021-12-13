872ff16d69d08f9f28c6e364ea01b0bc5670cf07
Michael Markus <joe.m.hesketh@gmail.com>
2021-12-13 15:49:24 +0000

    Finished lecture 10: Added AI behaviour to stop following once within range and within LOS.

--- Source/UnrealProject/AI/SBTService_CheckAttackRange.h
+++ Source/UnrealProject/AI/SBTService_CheckAttackRange.h
@@ -12,6 +12,17 @@
 UCLASS()
 class UNREALPROJECT_API USBTService_CheckAttackRange : public UBTService
 {
+
 	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	FBlackboardKeySelector AttackRangeKey;
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	float AttackRange = 2000.0f;
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 	
 };
